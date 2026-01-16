21730b5e4b2531db8f5f496691a5ee8d782de6ac
SharmaAryanOfficial <aryansharmaofficial22@gmail.com>
2026-01-16 10:24:26 +0530

    Addition of chat history

--- src/handlers/handler.py
+++ src/handlers/handler.py
@@ -4,7 +4,7 @@ from src.agents.chat_agent.states.chat_agent_state import ChatAgentState
 
 graph = create_chat_agent_graph()
 
-def chat_agent_handler(message : str) -> dict[str, str]:
+def chat_agent_handler(thread_id: str, message : str) -> ChatAgentState:
     """
     """
     return graph.invoke(
@@ -13,7 +13,32 @@ def chat_agent_handler(message : str) -> dict[str, str]:
         },
         config={
             'configurable':{
-                'thread_id': '1'
+                'thread_id': thread_id
                 }
         }
     ) #{'messages': answer from AI}
+
+
+
+def get_all_threads_handler() ->list[str | None]:
+    """
+    Docstring for get_all_threads_handler
+    """
+    all_checkpoints =  graph.checkpointer.list(config={})
+
+    threads = set()
+
+    for checkpoint in all_checkpoints:
+        threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list (threads)
+
+def chat_history_handler(thread_id: str) -> ChatAgentState | dict[None, None]:
+    """
+    Docstring for chat_history_handler
+    """
+    return graph.get_state(config={
+        'configurable': {
+            'thread_id': thread_id
+        }
+    })[0]
--- src/routes/route.py
+++ src/routes/route.py
@@ -1,13 +1,31 @@
 from fastapi import APIRouter
-from src.handlers.handler import chat_agent_handler
+from src.handlers.handler import chat_agent_handler, get_all_threads_handler, chat_history_handler
 from src.agents.chat_agent.states.chat_agent_state import ChatAgentState
 
 
 router = APIRouter()
 
 
-@router.post("/chat")
-def chat_agent_route(message: str)-> ChatAgentState:
+@router.post("/chat/{thread_id}")
+def chat_agent_route(thread_id: str, message: str)-> ChatAgentState:
     """
     """
-    return chat_agent_handler(message=message)
+    return chat_agent_handler(thread_id=thread_id, message=message)
+
+@router.get("/chat/threads")
+def get_all_threads() -> list[str | None]:
+    """
+    Docstring for get_all_threads
+    """
+    return get_all_threads_handler()
+
+
+@router.get('/chat/history/{thread_id}')
+def get_chat_history(thread_id: str) -> ChatAgentState | dict[None, None]:
+    """
+    Docstring for get_chat_history
+    
+    :param thread_id: Description
+    :type thread_id: str
+    """
+    return chat_history_handler(thread_id = thread_id)
